44faed1f577bafc07e4c784b38afe5925b6b127f
Arthur LECUREUR <e2204707@ensimlan.univ-lemans.fr>
2025-02-27 12:10:17 +0100

    Modify MainMenu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,8 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+
+    public GameObject MainMenu;
+    public GameObject AboutMenu
+    ;
+    void Start()
+    {
+        MainMenu.SetActive(true);
+        AboutMenu.SetActive(false);
+    }
+
     public void StartBtn()
     {
+        Debug.Log("StartBtnClick");
         SceneManager.LoadScene("asteroide");
+    }
+
+    public void ContinueBtn()
+    {
+        Debug.Log("ContinueBtnClick");
+    }
+
+    public void AboutBtn()
+    {
+        Debug.Log("AboutBtnClick");
+        MainMenu.SetActive(false);
+        AboutMenu.SetActive(true);
+    }
+
+    public void BackBtn()
+    {
+        Debug.Log("BackBtnClick");
+        MainMenu.SetActive(true);
+        AboutMenu.SetActive(false);
+    }
+
+    public void QuitBtn()
+    {
+        Debug.Log("QuitBtnClick");
+        Application.Quit();
     }
 }
