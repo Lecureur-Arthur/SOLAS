087caf83bc50558ee4d07b7d90c93c42c5a10ca5
Thibaut PLATET <e2302875@ensimlan.univ-lemans.fr>
2025-03-19 12:28:56 +0100

    MoonSurfaceEchellesPortesPresqueFonctionnelles

---
+++ Assets/Scripts/DoorMoon.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMoon : MonoBehaviour
+{
+    public AudioClip OpenDoor;
+    public AudioClip CloseDoor;
+
+    private AudioSource audioSource;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInParent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        Debug.Log("Start DoorTrigger");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("collision white player");
+            anim.SetBool("Character_nearby", true);
+            audioSource.PlayOneShot(OpenDoor);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player exit");
+            anim.SetBool("Character_nearby", false);
+            audioSource.PlayOneShot(CloseDoor);
+        }
+    }
+}
